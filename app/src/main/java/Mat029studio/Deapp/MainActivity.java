e1c8b911852c375a6fd5ab1acb21f0515a0ce854
Mat029 <boulabgerp@gmail.com>
2020-10-21 23:16:04 +0200

    Beta 1.0.0

--- app/src/main/java/Mat029studio/Deapp/MainActivity.java
+++ app/src/main/java/Mat029studio/Deapp/MainActivity.java
@@ -1,14 +1,80 @@
 package Mat029studio.Deapp;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import mat029studio.firstapp.deapp.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button6 = (Button) findViewById(R.id.b6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DiceActivity.class);
+                intent.putExtra("max", 6);
+                startActivity(intent);
+            }
+        });
+
+        Button button20 = (Button) findViewById(R.id.b20);
+        button20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DiceActivity.class);
+                intent.putExtra("max", 20);
+                startActivity(intent);
+            }
+        });
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                setContentView(R.layout.activity_main);
+
+                Button button6 = (Button) findViewById(R.id.b6);
+                button6.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, DiceActivity.class);
+                        intent.putExtra("max", 6);
+                        startActivity(intent);
+                    }
+                });
+
+                Button button20 = (Button) findViewById(R.id.b20);
+                button20.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, DiceActivity.class);
+                        intent.putExtra("max", 20);
+                        startActivity(intent);
+                    }
+                });
+                return true;
+            case R.id.action_settings:
+                setContentView(R.layout.activity_settings);
+                return true;
+            case R.id.action_search:
+                /* DO DELETE */
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
